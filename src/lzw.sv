90524d1f9de046014c0cb4ca399f1e28e7dac1b3
Colin Wu <cwu427@gatech.edu>
2024-02-06 20:39:21 -0500

    less bugs

--- src/lzw.sv
+++ src/lzw.sv
@@ -1,7 +1,7 @@
 // Design
 // lzw
 module lzw (
-    input clk,
+    input clk_i,
     input reset_i,
     output [13:0][11:0] compressed_o
 );
@@ -15,22 +15,24 @@ module lzw (
   
   state_t state, next_state;
   
-  logic [13:0][7:0] text;
+  logic [0:13][7:0] text;
   
   logic [7:0] clk_counter;
-  logic [7:0] counter = 0;
-  logic [7:0] CALC_counter = 0;
+  logic [7:0] counter, next_counter;
+  logic [7:0] CALC_counter, next_CALC_counter;
   logic [31:0] sps;
   logic [31:0] str;
   logic [31:0][11:0] dictionary, next_dictionary;
   logic [11:0] dictionary_size, next_dictionary_size;
   logic [11:0] symbol;
-  logic [13:0][11:0] compressed; 
+  logic [13:0][11:0] compressed, next_compressed; 
   
   integer i;
 
-  always_ff @ (posedge clk or posedge reset_i) begin
+  always_ff @ (posedge clk_i or posedge reset_i) begin
     if (reset_i) begin
+      counter <= 0;
+      CALC_counter <= 0;
       clk_counter <= 0;
       state <= RESET;
       text <= '{ 
@@ -63,14 +65,17 @@ module lzw (
     	// "a"
     	8'b01100001
   	  };
-      dictionary_size = 256;
+      dictionary_size <= 256;
     end
     else begin
-      clk_counter <= counter + 1;
+      clk_counter <= clk_counter + 1;
       state <= next_state;
       text <= text;
-      dictionary_size = next_dictionary_size;
-      dictionary = next_dictionary;
+      dictionary_size <= next_dictionary_size;
+      dictionary <= next_dictionary;
+      CALC_counter <= next_CALC_counter;
+      counter <= next_counter;
+      compressed <= next_compressed;
     end
   end
   
@@ -78,6 +83,12 @@ module lzw (
     case (state)
       RESET: begin
         next_state = CALC;
+        next_dictionary_size = 256;
+        for (i = 0; i < 256; i++) begin
+          next_dictionary[i] = i;
+  	    end
+        next_counter = 0;
+        next_CALC_counter = 0;
       end
       CALC: begin
         if (CALC_counter == 14) next_state = DONE;
@@ -85,16 +96,16 @@ module lzw (
         sps = (str << 8) + symbol;
         if(dictionary[sps] != 0) begin
           str = sps;
-          next_state <= CALC;
+          next_state = CALC;
         end
         else begin
-          compressed[counter] = dictionary[str];
-          counter = counter + 1;
+          next_compressed[counter] = dictionary[str];
+          next_counter = counter + 1;
           next_dictionary[sps] = dictionary_size;
           next_dictionary_size = dictionary_size + 1;
           str = symbol;
         end
-        CALC_counter = CALC_counter + 1;
+        next_CALC_counter = CALC_counter + 1;
       end
       DONE: begin
         next_state = DONE;
