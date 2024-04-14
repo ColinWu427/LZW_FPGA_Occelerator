c2311cfbdfc6a32da91b7119c9d7b32746d9033f
adecesaris <164797157+adecesaris@users.noreply.github.com>
2024-04-14 10:54:45 -0400

    Add files via upload

--- conflict_table.sv
+++ conflict_table.sv
@@ -10,13 +10,17 @@ module conflict_table
     output match,
     input [DATA_WIDTH-1:0] data,
     input [HASH_WIDTH-1:0] hash_in,
+    input [HASH_WIDTH-1:0] map_in,
     output [HASH_WIDTH-1:0] hash_out,
+    output [HASH_WIDTH-1:0] map_out,
     output ct_full
   );
 
   reg [HASH_WIDTH-1:0] tmp_hash;
+  reg [HASH_WIDTH-1:0] tmp_map;
   reg [DATA_WIDTH-1:0] mem_data [DEPTH];
   reg [HASH_WIDTH-1:0] mem_hash [DEPTH];
+  reg [HASH_WIDTH-1:0] mem_map [DEPTH];
   wire [DEPTH-1:0] match_wires;
   reg [HASH_WIDTH-1:0] counter;
   wire [7:0] encoder_in;
@@ -34,19 +38,31 @@ module conflict_table
 */
 
 // Match wires
-  assign  match_wires[7] = (data == mem_data[7]) ? 1'b1 : 1'b0;
-  assign  match_wires[6] = (data == mem_data[6]) ? 1'b1 : 1'b0;
-  assign  match_wires[5] = (data == mem_data[5]) ? 1'b1 : 1'b0;
-  assign  match_wires[4] = (data == mem_data[4]) ? 1'b1 : 1'b0;
-  assign  match_wires[3] = (data == mem_data[3]) ? 1'b1 : 1'b0;
-  assign  match_wires[2] = (data == mem_data[2]) ? 1'b1 : 1'b0;
-  assign  match_wires[1] = (data == mem_data[1]) ? 1'b1 : 1'b0;
-  assign  match_wires[0] = (data == mem_data[0]) ? 1'b1 : 1'b0;
+  assign  match_wires[7] = ((data == mem_data[7]) & (data != 0)) ? 1'b1 : 1'b0;
+  assign  match_wires[6] = ((data == mem_data[6]) & (data != 0)) ? 1'b1 : 1'b0;
+  assign  match_wires[5] = ((data == mem_data[5]) & (data != 0)) ? 1'b1 : 1'b0;
+  assign  match_wires[4] = ((data == mem_data[4]) & (data != 0)) ? 1'b1 : 1'b0;
+  assign  match_wires[3] = ((data == mem_data[3]) & (data != 0)) ? 1'b1 : 1'b0;
+  assign  match_wires[2] = ((data == mem_data[2]) & (data != 0)) ? 1'b1 : 1'b0;
+  assign  match_wires[1] = ((data == mem_data[1]) & (data != 0)) ? 1'b1 : 1'b0;
+  assign  match_wires[0] = ((data == mem_data[0]) & (data != 0)) ? 1'b1 : 1'b0;
 
 // Encoder logic
   assign encoder_out[2] = encoder_in[4] | encoder_in[5] | encoder_in[6] | encoder_in[7];
   assign encoder_out[1] = encoder_in[2] | encoder_in[3] | encoder_in[6] | encoder_in[7];
   assign encoder_out[0] = encoder_in[1] | encoder_in[3] | encoder_in[5] | encoder_in[7];
+
+  initial begin
+    counter <= 0;
+    mem_data[7] <= 'h0;
+    mem_data[6] <= 'h0;
+    mem_data[5] <= 'h0;
+    mem_data[4] <= 'h0;
+    mem_data[3] <= 'h0;
+    mem_data[2] <= 'h0;
+    mem_data[1] <= 'h0;
+    mem_data[0] <= 'h0;
+  end
 
 // Reset for counter
   always @ (posedge clk) begin
@@ -63,32 +79,20 @@ module conflict_table
     end
   end
 
-  initial begin
-    counter <= 0;
-    mem_data[7] <= 'h0;
-    mem_data[6] <= 'h0;
-    mem_data[5] <= 'h0;
-    mem_data[4] <= 'h0;
-    mem_data[3] <= 'h0;
-    mem_data[2] <= 'h0;
-    mem_data[1] <= 'h0;
-    mem_data[0] <= 'h0;
-  end
-
-// Writing entry into conflict table
   always @ (posedge clk) begin
-    if (cs & we) begin
+// Writing entry into conflict table
+    if (cs & we & rst) begin
 	mem_data[counter] <= data;
 	mem_hash[counter] <= hash_in;
+	mem_map[counter] <= map_in;
 	if (counter != 3'b111)
 	  counter <= counter + 1;
     end
-  end
-
 // Getting entry from conflict table
-  always @ (posedge clk) begin
-    if (cs & !we)
+    else if (cs & !we & rst) begin
 	tmp_hash <= mem_hash[encoder_out];
+	tmp_map <= mem_map[encoder_out];
+    end
   end
 	  	
 // Assert match when any of the data cells match the input
@@ -97,6 +101,8 @@ module conflict_table
   assign encoder_in[7:0] = match_wires[7:0];
 // Hash output
   assign hash_out = cs & !we ? tmp_hash : 'hz;
+// Map output
+  assign map_out = cs & !we ? tmp_map : 'hz;
 // If the counter is at 8, the conflict table is full
   assign ct_full = (counter == 3'b111) ? 1 : 0;
 
